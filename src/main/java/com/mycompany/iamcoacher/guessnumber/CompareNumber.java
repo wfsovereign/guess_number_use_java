7cb606c5ec3951ae140a6c78888de8ad45cd44be
wfsovereign <never_fall@sohu.com>
2015-03-13 10:33:06 +0800

    use real logic replace quick implement

--- src/main/java/com/mycompany/iamcoacher/guessnumber/CompareNumber.java
+++ src/main/java/com/mycompany/iamcoacher/guessnumber/CompareNumber.java
@@ -5,7 +5,13 @@ package com.mycompany.iamcoacher.guessnumber;
  */
 public class CompareNumber {
     public String getTips(String input,String answer){
-        if(input.equals(answer)){
+        int aCount = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if(answer.charAt(i) == input.charAt(i)){
+                aCount++;
+            }
+        }
+        if(aCount == 4 ){
             return "4A0B";
         }
         int containCount = 0;
