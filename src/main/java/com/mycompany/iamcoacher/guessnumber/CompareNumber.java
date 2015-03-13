5d86910a0333041eb9b7909330a971c8d9758b93
wfsovereign <never_fall@sohu.com>
2015-03-13 10:47:33 +0800

    refactor ,remove useless for

--- src/main/java/com/mycompany/iamcoacher/guessnumber/CompareNumber.java
+++ src/main/java/com/mycompany/iamcoacher/guessnumber/CompareNumber.java
@@ -6,13 +6,12 @@ package com.mycompany.iamcoacher.guessnumber;
 public class CompareNumber {
     public String getTips(String input,String answer){
         int aCount = 0;
+        int containCount = 0;
+
         for (int i = 0; i < input.length(); i++) {
             if(answer.charAt(i) == input.charAt(i)){
                 aCount++;
             }
-        }
-        int containCount = 0;
-        for (int i = 0; i < input.length(); i++) {
             if(answer.contains(String.valueOf(input.charAt(i)))){
                 containCount++;
             }
