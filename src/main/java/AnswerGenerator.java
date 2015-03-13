24dbac9e02e4f58ce67d6433b50263bc9aaa0732
wfsovereign <never_fall@sohu.com>
2015-03-13 11:34:42 +0800

    rafactor, use real logic replace simple implement

--- src/main/java/AnswerGenerator.java
+++ src/main/java/AnswerGenerator.java
@@ -1,8 +1,18 @@
+import java.util.Random;
+
 /**
  * Created by wfsovereign on 15-3-13.
  */
 public class AnswerGenerator {
     public String generare() {
-        return "1234";
+        Random random = new Random();
+        StringBuilder result = new StringBuilder();
+        while (result.length()<4){
+            String digit = String .valueOf(random.nextInt(10));
+            if(result.indexOf(digit) == -1){
+                result.append(digit);
+            }
+        }
+        return result.toString();
     }
 }
