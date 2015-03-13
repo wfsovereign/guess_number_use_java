9dab64268492a5eb6a3a995da739090b7870b10e
wfsovereign <never_fall@sohu.com>
2015-03-13 14:45:04 +0800

    no repeat answer in 3 times

--- src/main/java/AnswerGenerator.java
+++ src/main/java/AnswerGenerator.java
@@ -1,11 +1,36 @@
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by wfsovereign on 15-3-13.
  */
+
+
+
 public class AnswerGenerator {
-    public String generare() {
-        Random random = new Random();
+    private final List<String> historyGenerateString;
+    private Random random;
+
+    public AnswerGenerator(Random random) {
+
+        this.random = random;
+        this.historyGenerateString = new ArrayList<String >();
+    }
+
+    public String generate() {
+        String result = generateAnswerOnce();
+        while (historyGenerateString.contains(result)){
+            result = generateAnswerOnce();
+        }
+        if (historyGenerateString.size() == 2){
+            historyGenerateString.remove(0);
+        }
+        historyGenerateString.add(result);
+        System.out.println("==============");
+//        System.out.println(historyGenerateString);
+        return result;
+    }
+
+    private String generateAnswerOnce() {
         StringBuilder result = new StringBuilder();
         while (result.length()<4){
             String digit = String .valueOf(random.nextInt(10));
--- src/test/java/AnswerGeneratorTest.java
+++ src/test/java/AnswerGeneratorTest.java
@@ -1,7 +1,13 @@
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.fest.assertions.api.Assertions.fail;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by wfsovereign on 15-3-13.
@@ -9,7 +15,7 @@ import static org.fest.assertions.api.Assertions.fail;
 public class AnswerGeneratorTest {
     @Test
     public void should_generate_4_digits_string(){
-        String result = new AnswerGenerator().generare();
+        String result = new AnswerGenerator(new Random()).generate();
 
         assertThat(result.length()).isEqualTo(4);
 
@@ -17,7 +23,7 @@ public class AnswerGeneratorTest {
 
     @Test
     public void should_generate_number(){
-        String result = new AnswerGenerator().generare();
+        String result = new AnswerGenerator(new Random()).generate();
 
         try {
             Integer.parseInt(result);
@@ -28,11 +34,52 @@ public class AnswerGeneratorTest {
 
     @Test
     public void should_generate_no_duplicated_digits_string(){
-        String result = new AnswerGenerator().generare();
+        String result = new AnswerGenerator(new Random()).generate();
 
         for (int i = 0; i < result.length(); i++) {
             assertThat(result.indexOf(result.charAt(i))).isEqualTo(result.lastIndexOf(result.charAt(i)));
         }
+    }
+
+    @Test
+    public void should_not_repeat_in_3_times(){
+        Random random = mock(Random.class) ;
+        given(random.nextInt(10))
+                .willReturn(1,2,3,4)
+                .willReturn(1,2,3,4)
+                .willReturn(2,3,4,5)
+                .willReturn(3,4,5,6);
+        AnswerGenerator answerGenerator = new AnswerGenerator(random);
+        Set<String > results = new HashSet<String >();
+
+
+        results.add(answerGenerator.generate());
+        results.add(answerGenerator.generate());
+        results.add(answerGenerator.generate());
+
+        assertThat(results.size()).isEqualTo(3);
+
+    }
+
+
+    @Test
+    public void should_result_repeatable_out_of_3_times(){
+        Random random = mock(Random.class) ;
+        given(random.nextInt(10))
+                .willReturn(1,2,3,4)
+                .willReturn(1,4,6,7)
+                .willReturn(2,3,4,5)
+                .willReturn(1,2,3,4);
+        AnswerGenerator answerGenerator = new AnswerGenerator(random);
+        Set<String > results = new HashSet<String >();
+
+
+        results.add(answerGenerator.generate());
+        results.add(answerGenerator.generate());
+        results.add(answerGenerator.generate());
+
+        assertThat(results.size()).isEqualTo(3);
+
     }
 
 
